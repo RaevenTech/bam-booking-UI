1946d3dbc019d8dc5d4a924186efbbee21fe7c25
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-20 15:14:46 +0100

    search

--- bam-ui/src/features/header/HeaderSearch.js
+++ bam-ui/src/features/header/HeaderSearch.js
@@ -1,7 +1,60 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faMapLocation,
+    faCalendarDays,
+    faUserGroup,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./headerSearch.module.css";
+//import { useState } from "react";
 
 const HeaderSearch = () => {
-    return <div className={styles.header_search}>HeaderSearch</div>;
+    //const [searchLocation, setSearchLocation] = useState("");
+
+    return (
+        <>
+            <div className={styles.header_search}>
+                <div className={styles.header_search_container}>
+                    <div className={styles.header_search_section}>
+                        {" "}
+                        <FontAwesomeIcon
+                            icon={faMapLocation}
+                            className={styles.header_search_icon}
+                        />
+                        <input
+                            className={styles.header_search_input}
+                            type="text"
+                            placeholder="Search by location"
+                            //onChange={(e) => setSearchLocation(e.target.value)}
+                        />
+                    </div>
+                    <div className={styles.header_search_section}>
+                        <FontAwesomeIcon
+                            icon={faCalendarDays}
+                            className={styles.header_search_icon}
+                        />
+                        <span className={styles.header_search_text}>
+                            date in - date out
+                        </span>
+                    </div>
+                    <div className={styles.header_search_section}>
+                        {" "}
+                        <FontAwesomeIcon
+                            icon={faUserGroup}
+                            className={styles.header_search_icon}
+                        />{" "}
+                        <span className={styles.header_search_text}>
+                            2 adults . 0 children . 1 room{" "}
+                        </span>
+                    </div>
+                    <div className={styles.header_search_section}>
+                        <button className={styles.header_search_button}>
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default HeaderSearch;
