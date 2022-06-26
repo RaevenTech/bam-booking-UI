1b95b01eaa222483ba0598f47b298f79a8c78eaa
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-26 17:48:09 +0100

    secondary city

--- bam-ui/src/features/city/FeaturedCity.js
+++ bam-ui/src/features/city/FeaturedCity.js
@@ -38,6 +38,32 @@ const FeaturedCity = () => {
                     </div>
                 </div>
             </div>
+            <section>
+                <div className={styles.cities_secondary}>
+                    <div className={styles.cities_item}>
+                        <img
+                            className={styles.cities_img}
+                            src="http://placeimg.com/640/360/any"
+                            alt="city"
+                        />
+                        <div className={styles.cities_title}>
+                            <h1>City Name</h1>
+                            <h3>Subtitle</h3>
+                        </div>
+                    </div>
+                    <div className={styles.cities_item}>
+                        <img
+                            className={styles.cities_img}
+                            src="http://placeimg.com/640/360/any"
+                            alt="city"
+                        />
+                        <div className={styles.cities_title}>
+                            <h1>City Name</h1>
+                            <h3>Subtitle</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
