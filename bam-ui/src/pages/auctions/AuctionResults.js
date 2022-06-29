f6fa477889b4cefe8b7991b924101631dc31bdf7
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-06-29 20:01:22 +0100

    styling the second page search results

--- bam-ui/src/pages/auctions/AuctionResults.js
+++ bam-ui/src/pages/auctions/AuctionResults.js
@@ -4,12 +4,41 @@ const AuctionResults = () => {
     return (
         <div className={styles.auction_results}>
             <img
-                className={styles.search_results_list}
+                className={styles.search_results_img}
                 src="https://www.fillmurray.com/640/360"
                 alt=""
             />
             <div className={styles.results_description}>
-                <h1 className={styles.title}>Apartment</h1>
+                <h1 className={styles.results_title}>Apartment</h1>
+                <div className={styles.results_attraction}>
+                    5min walk to the beach
+                </div>
+                <div className={styles.results_features}>
+                    Apartment has Air conditioning
+                </div>
+                <div className={styles.results_description_room}>
+                    2 Bed, 1 Bath, full Kitchen
+                </div>
+                <div className={styles.results_terms_conditions}>
+                    <small>Once the bidding id closed, NO cancellations</small>
+                </div>
+            </div>
+            <div className={styles.details}>
+                <div className={styles.details_rating}>
+                    <span>Excellent</span>
+                    <button className={styles.details_rating_btn}>8.7</button>
+                </div>
+                <div className={styles.results_details_prices}>
+                    <span className={styles.results_details_purchase_price}>
+                        €55
+                    </span>
+                    <span className={styles.results_details_starting_price}>
+                        €10
+                    </span>
+                    <span className={styles.results_details_current_price}>
+                        €23
+                    </span>
+                </div>
             </div>
         </div>
     );
