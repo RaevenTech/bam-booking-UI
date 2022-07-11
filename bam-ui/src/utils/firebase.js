5f84614c5db9590d7c6869af69463697761cff30
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-07-11 10:32:40 +0100

    firebase
    attempting to create a Crud using new firebase hooks

---
+++ bam-ui/src/utils/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2Q8kKXYsgH8zKpVtQtEPkstvk9GwAEKE",
+    authDomain: "bid2buy-ca5c9.firebaseapp.com",
+    databaseURL: "https://bid2buy-ca5c9-default-rtdb.firebaseio.com",
+    projectId: "bid2buy-ca5c9",
+    storageBucket: "bid2buy-ca5c9.appspot.com",
+    messagingSenderId: "675775514523",
+    appId: "1:675775514523:web:892b453c887cd81302433e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
