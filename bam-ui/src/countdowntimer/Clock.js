98402751108ecabe537424b0adef3946f820e9ec
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-07-15 19:34:00 +0100

    counter comp created
    not functional

--- bam-ui/src/countdowntimer/Clock.js
+++ bam-ui/src/countdowntimer/Clock.js
@@ -1,32 +1,30 @@
 import React from "react";
-import styles from "./clock.module.css"
+import styles from "./clock.module.css";
 
 const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
     //props come from the live auction
-    return (  
-     <div className={styles.timer_container}>
-        <div className={styles.timer}>
-            <section className={styles.timer_item}>
-                <div>{timerDays}00</div>
-                <small>Days</small>
-            </section>
-            <section className={styles.timer_item}>
-                <div>{timerHours}00</div>
-                <small>:</small>
-            </section>
-            <section className={styles.timer_item}>
-                <div>{timerMinutes}00</div>
-                <small>:</small>
-            </section>
-            <section className={styles.timer_item}>
-                <div>{timerSeconds}00</div>
-                <small>:</small>
-            </section>
+    return (
+        <div className={styles.timer_container}>
+            <div className={styles.timer}>
+                <section className={styles.timer_item}>
+                    <div>{timerDays}00</div>
+                    <small>Days</small>
+                </section>
+                <section className={styles.timer_item}>
+                    <div>{timerHours}00</div>
+                    <small>hrs</small>
+                </section>
+                <section className={styles.timer_item}>
+                    <div>{timerMinutes}00</div>
+                    <small>min</small>
+                </section>
+                <section className={styles.timer_item}>
+                    <div>{timerSeconds}00</div>
+                    <small>sec</small>
+                </section>
+            </div>
         </div>
-     </div>
-   
-    )
-   
+    );
 };
 
 export default Clock;
