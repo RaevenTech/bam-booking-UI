ffd524d9fbc7e0598724456d6f2244164d2f8808
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-07-21 18:36:23 +0100

    some how created a infinite URLSearchParams

--- bam-ui/src/features/navbar/Navbar.js
+++ bam-ui/src/features/navbar/Navbar.js
@@ -1,21 +1,17 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { firebaseAuth } from "../../utils/firebase";
 
 const Navbar = ({ logOut, page }) => {
     const navigate = useNavigate();
-    const [User, setUser] = useState(undefined);
-
-    const handleForms = () => {
-        navigate("/login/register");
-    };
+    const [user, setUser] = useState(undefined);
 
     onAuthStateChanged(firebaseAuth, (currentUser) => {
-        if (currentUser) setUser(currentUser);
+        if (currentUser) );
         else navigate("/");
     });
 
@@ -26,30 +22,27 @@ const Navbar = ({ logOut, page }) => {
                     <div className={styles.nav_logo}>
                         Bid<span className={styles.logo_2}>2</span>Buy.com
                     </div>
-                    {(logOut !== "details") | (page !== "auctions") && (
+
+                    {logOut !== "details" && page !== "auction" && (
                         <div className={styles.nav_btns}>
-                            <button
-                                className={styles.nav_btn}
-                                onClick={handleForms}
-                            >
-                                Register
-                            </button>
-                            <button
-                                className={styles.nav_btn}
-                                onClick={handleForms}
-                            >
-                                Sign in
-                            </button>{" "}
-                            :
-                            <FontAwesomeIcon
-                                icon={faUserCircle}
-                                className={styles.user_icon}
-                            />
-                            <button onClick={() => signOut(firebaseAuth)}>
-                                Log Out
-                            </button>
+                            <button className={styles.nav_btn}>Register</button>
+                            <button className={styles.nav_btn}>Sign in</button>
                         </div>
                     )}
+                    <>
+                        {logOut === "details" && (
+                            <div className={styles.sign_out}>
+                                <h2>{user?.email}</h2>
+                                <FontAwesomeIcon
+                                    icon={faUserCircle}
+                                    className={styles.user_icon}
+                                />
+                                <button onClick={() => signOut(firebaseAuth)}>
+                                    Log Out
+                                </button>
+                            </div>
+                        )}
+                    </>
                 </div>
             </div>
         </nav>
